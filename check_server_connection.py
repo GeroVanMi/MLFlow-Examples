cf709c54651e4d791094025b96e5b82171f6b0e1
Gerome Meyer <gerome.meyer@protonmail.com>
2024-03-13 10:48:15 +0100

    Renamed files

--- check_server_connection.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-
-mlflow.set_tracking_uri("http://localhost:5000")
-
-mlflow.set_experiment("/tracking_server_tutorial")
-
-with mlflow.start_run():
-    mlflow.log_metric("foo", 1)
-    mlflow.log_metric("bar", 2)
